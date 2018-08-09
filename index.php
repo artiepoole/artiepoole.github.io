2a9fd1cdba06b4e266ea5104b712ac3fa312cb6d
StuPoole <stuart.f.poole@gmail.com>
2018-08-09 14:46:50 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Home - Nottingham Nanoscience</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <style>
+            body {
+                padding-top: 80px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40792531-1', 'nottingham.ac.uk');
+  ga('send', 'pageview');
+
+</script>
+
+    </head>
+    <body>
+
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar pull-left" data-toggle="collapse" data-target=".nav-collapse">
+						<i class="icon-reorder"></i>
+                    </a>
+                    
+                    <a class="pull-right" href="http://www.nottingham.ac.uk"><img src="img/uon_logo_202x60.png" style="margin:10px 10px"></a>
+
+                    <!--<a class="brand" href="./index.php">Nottingham Nanoscience</a>-->
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="./index.php">Home</a></li>
+							<li><a href="./research.php">Research</a></li>
+							<li><a href="./publications.php">Publications</a></li>
+							<li><a href="./people.php">People</a></li>
+							<li><a href="./news.php">News</a></li>
+                            <li><a href="./events.php">Events</a></li>
+							<li><a href="./outreach.php">Outreach</a></li>
+							<li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">More <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="./vacancies.php">Vacancies</a></li>                                    
+									<li><a href="./links.php">Links</a></li>
+									<li><a href="./contact.php">Contact</a></li>
+									<li><a href="./wiki/">Wiki</a></li>
+									<li><a target="_blank" href="http://www.nottingham.ac.uk/~ppzlmap/nanoforum/cgi-bin/forum/">Forum</a></li>
+									<li><a href="./financial.php">Final Grant Reports</a></li>
+                                </ul>
+                            </li>
+                            
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div><!--Navbar-->
+
+		<div class="container">
+
+		<div id="banner" class="carousel slide hidden-phone" data-interval="7500" style="margin-top:10px">
+		  <!-- Carousel items -->
+		  <div class="carousel-inner">
+			<div class="active item">
+				<img src="img/banner_scanning.jpg" alt="An STM image of the silicon 111 surface">		
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Welcome</h1>
+						<p class="lead">to the Nottingham Nanoscience group website</p>
+						<br>
+						<p>The group is part of the <a target="_blank" href="http://www.nottingham.ac.uk/physics/index.aspx">School of Physics and Astronomy</a> at the University of Nottingham, UK.</p>
+						
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="img/banner_disorderedCrystal.jpg" alt="A disordered crystal">
+				<div class="container">
+					<div class="carousel-caption">
+						<p class="lead">Our research interests</p> 
+						<p>...span a range of exciting and topical themes in state-of-the-art nanoscale science. Our core activities are focused on understanding and controlling the structure and behaviour of condensed matter on length scales ranging from the single molecule limit to micron dimensions.</p>
+				        <p><a class="btn" href="./research.php">Learn more <i class="icon-double-angle-right"></i></a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="img/banner_semicrystallinePolymer.jpg" alt="A semicrystalline polymer">
+				<div class="container">
+					<div class="carousel-caption">
+						<p class="lead">We are housed</p> 
+						<p>... in a suite of recently refurbished laboratories containing an excellent equipment base. Since 2007 we have partnered with the multidisciplinary Nanotechnology and Nanoscience Centre, based in the School of Physics, which promotes the development of many exciting intra- and inter-University collaborations.</p>
+				    </div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="img/banner_algorithmicProgression.jpg" alt="An example of algorithmic improvement of SPM imaging">
+				<div class="container">
+					<div class="carousel-caption">
+						<p class="lead">Collaborations</p> 
+						<p>... by the group have led to many exciting new developments. This image, for example, shows a series of STM scans that demonstrate a genetic algorithm designed by the group in collaboration with the Computer Science School(UoN) and the Beckman Institute (Illinois) to automatically optimize imaging parameters. This work was recently honoured at the 2012 Human-Competitive awards</p>
+						<p><a class="btn" href="./news.php#HumanCompAward">Learn more <i class="icon-double-angle-right"></i></a></p>
+				    </div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="img/banner_colouredSpherulites.jpg" alt="An image of giant amyloid spherulites">
+				<div class="container">
+					<div class="carousel-caption">
+						<p class="lead">World leading research</p> 
+						<p>... performed by the group in a variety of areas enables our track record of high quality publications. The image shown here was featured on the cover of the RSC journal, Soft Matter. It shows the beautiful rings formed when light is passed through giant amyloid spherulites formed from bovine insulin.</p>
+						<p><a class="btn" href="./research.php#softMatter">Learn more <i class="icon-double-angle-right"></i></a></p>
+				    </div>
+				</div>
+			</div>
+			<!--<div class="item">
+				<img src="img/banner_fibroplastCells.jpg" alt="An image of fibroplast cells">
+				<div class="container">
+					<div class="carousel-caption">
+						<p class="lead">This image</p> 
+						<p>... shows some 3T3 fibroplast cells on a wrinkled polystyrene surface. The cells have been chemically stained and imaged using flourescence optical microscopy.</p>
+						<p><a class="btn" href="./research.php#softMatter">Learn more <i class="icon-double-angle-right"></i></a></p>
+				    </div>
+				</div>
+			</div>-->
+		  </div>
+		  
+		  <ol class="carousel-indicators">
+			<li data-target="#banner" data-slide-to="0" class="active"></li>
+			<li data-target="#banner" data-slide-to="1"></li>
+			<li data-target="#banner" data-slide-to="2"></li>
+			<li data-target="#banner" data-slide-to="3"></li>
+			<li data-target="#banner" data-slide-to="4"></li>
+			<!--<li data-target="#banner" data-slide-to="5"></li>-->
+		  </ol>
+		  
+		  <!-- Carousel nav -->
+		  <a class="carousel-control left" href="#banner" data-slide="prev"><i class="icon-angle-left"></i></a>
+		  <a class="carousel-control right" href="#banner" data-slide="next"><i class="icon-angle-right"></i></a>
+		</div>
+
+		<div class="visible-phone">
+		  	<h2>Welcome</h2>
+		  	<p class="lead">To the Nottingham Nanoscience group website</p>
+			<p>The group is part of the <a href="http://www.nottingham.ac.uk/physics/">School of Physics and Astronomy</a> at the <a href="http://www.nottingham.ac.uk">University of Nottingham</a>, UK. Our research interests span a range of exciting and topical themes in state-of-the-art nanoscale science. Housed in a suite of recently refurbished laboratories containing an excellent equipment base, our core activities are focused on understanding and controlling the structure and behaviour of condensed matter on length scales ranging from the single molecule limit to micron dimensions.</p>
+		</div>
+
+		</div>
+
+		<div class="container">
+			
+		<hr>
+	
+		<div class="row">
+		
+			<div class="span5 offset1">
+			<div class="row">
+				<div class="span1">
+					<h3><i class="icon-book icon-2x"></i></h3>
+				</div>
+				<div class="span3">
+					<h3>Recent Publications</h3>
+				</div>
+			</div>
+
+			<div class="row">
+				<div style="overflow-y:auto">
+				<table class="table table-striped" width="90%">
+				<?php include 'scripts/publications_retrieve.php'; ?>
+				</table>
+				<p>A full list of publications can be found <a href="./publications.php">here</a>.
+				</p>
+				</div>
+			</div>
+			</div>
+
+			<div class="span5 offset1">
+			<div class="row">
+				<div class="span1">
+					<h3><i class="icon-bullhorn icon-2x"></i></h3>
+				</div>
+				<div class="span3">
+					<h3>Latest News</h3>
+				</div>
+			</div>
+
+			<div class="row">
+				<div style="overflow-y:auto">
+				<table class="table table-striped" width="90%">
+				<?php include 'scripts/news_retrieve.php'; ?>
+				</table>
+				<p>A full list of news items can be found <a href="./news.php">here</a>.
+				</p>
+				</div>
+			</div>
+			</div>
+				   		   
+		</div>
+
+
+		</div>		
+		
+		<?php include 'footer.html'; ?>
+
+    </body>
+</html>
